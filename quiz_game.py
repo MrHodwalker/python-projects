eafde1addfe49ec8ad6f4d958a0f859566ddce30
mrhodwalker <mhodwalkerl@outlook.com>
2024-03-02 13:15:14 -0500

    all questions added for the quiz

---
+++ quiz_game.py
@@ -0,0 +1,55 @@
+print("Welcome to Summoner's Quiz!")
+playing = input("Do you want to play? ")
+if playing != "yes":
+    quit()
+print("Okay! Let's destroy the enemies Nexus!")
+answer = input("What is the name of the continent where the city of Demacia and Noxus are located? ")
+if answer == "valoran":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Which champion's weapon of choice is a rocket launcher? ")
+if answer == "jinx":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Who is the only champion that can truly revive another champion? ")
+if answer == "zilean":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Which champion yells 'DEMACIA!' as he charges into battle? ")
+if answer == "garen":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Who is the Undead Juggernaut? ")
+if answer == "sion":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Which two champions are famously known as the 'Bird and the Branch'? ")
+if answer == "xayah and rakan":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("What is the name of the river that runs through the center of Summoner's Rift? ")
+if answer == "the serpentine river":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Which champion is known as the 'Chain Warden'? ")
+if answer == "thresh":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("What is the name of Yasuo's brother? ")
+if answer == "yone":
+    print("Correct!")
+else:
+    print("Incorrect!")
+answer = input("Who was the first champion ever designed? ")
+if answer == "singed":
+    print("Correct!")
+else:
+    print("Incorrect!")
